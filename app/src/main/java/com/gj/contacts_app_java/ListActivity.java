988631ed7c0561bb7878d696413feff4a0bc452f
Gabriel Fardoski <43648010+GabrielGPF@users.noreply.github.com>
2021-03-24 23:55:02 -0300

    Created Add Item activity layout

--- app/src/main/java/com/gj/contacts_app_java/ListActivity.java
+++ app/src/main/java/com/gj/contacts_app_java/ListActivity.java
@@ -2,7 +2,9 @@ package com.gj.contacts_app_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+    }
+
+    public void addItem(View view) {
+        Intent intent = new Intent(this, AddItemActivity.class);
+        startActivityForResult(intent, 0);
+//        startActivity(intent);
     }
 }
--- app/src/main/java/com/gj/contacts_app_java/MainActivity.java
+++ app/src/main/java/com/gj/contacts_app_java/MainActivity.java
@@ -15,37 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     public void login(View view) {
-        String editTextTextPersonName = ((EditText) findViewById(R.id.editTextTextPersonName)).getText().toString();
-        String editTextTextPassword = ((EditText) findViewById(R.id.editTextTextPassword)).getText().toString();
-
-        if(editTextTextPersonName.trim().length() > 0 && !editTextTextPersonName.isEmpty() &&
-                editTextTextPassword.trim().length() > 0 && !editTextTextPassword.isEmpty()){
-            if(editTextTextPersonName.equals("Gabriel") && editTextTextPassword.equals("secret")){
-                Intent intent = new Intent(this, ListActivity.class);
-                startActivity(intent);
-            } else {
-//                showWrongCredentialsDialog();
-            }
-        } else {
-//            showEmptyFieldsDialog();
-        }
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
     }
 
-    public void signIn(View view) {
-//        Log.i("text", "SIGN IN");
-
-        String editTextTextPersonName = ((EditText) findViewById(R.id.editTextTextPersonName)).getText().toString();
-        String editTextTextPassword = ((EditText) findViewById(R.id.editTextTextPassword)).getText().toString();
-
-        if(editTextTextPersonName.trim().length() > 0 && !editTextTextPersonName.isEmpty() &&
-                editTextTextPassword.trim().length() > 0 && !editTextTextPassword.isEmpty()){
-//            Intent intent = new Intent(this, Gallery.class);
-//            intent.putExtra("EXTRA_USERNAME", editTextTextPersonName);
-//            startActivity(intent);
-        } else {
-//            showEmptyFieldsDialog();
-        }
-    }
+    public void signIn(View view) {}
 }
