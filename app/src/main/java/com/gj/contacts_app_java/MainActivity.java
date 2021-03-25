fcec5af824083c3d2df6f2e5564cb28e21b48f67
Gabriel Fardoski <43648010+GabrielGPF@users.noreply.github.com>
2021-03-24 22:35:33 -0300

    Created Login page components

--- app/src/main/java/com/gj/contacts_app_java/MainActivity.java
+++ app/src/main/java/com/gj/contacts_app_java/MainActivity.java
@@ -2,7 +2,10 @@ package com.gj.contacts_app_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void login(View view) {
+        String editTextTextPersonName = ((EditText) findViewById(R.id.editTextTextPersonName)).getText().toString();
+        String editTextTextPassword = ((EditText) findViewById(R.id.editTextTextPassword)).getText().toString();
+
+        if(editTextTextPersonName.trim().length() > 0 && !editTextTextPersonName.isEmpty() &&
+                editTextTextPassword.trim().length() > 0 && !editTextTextPassword.isEmpty()){
+            if(editTextTextPersonName.equals("Gabriel") && editTextTextPassword.equals("secret")){
+//                Intent intent = new Intent(this, Gallery.class);
+//                intent.putExtra("EXTRA_USERNAME", editTextTextPersonName);
+//                startActivity(intent);
+            } else {
+//                showWrongCredentialsDialog();
+            }
+        } else {
+//            showEmptyFieldsDialog();
+        }
+    }
+
+    public void signIn(View view) {
+//        Log.i("text", "SIGN IN");
+
+        String editTextTextPersonName = ((EditText) findViewById(R.id.editTextTextPersonName)).getText().toString();
+        String editTextTextPassword = ((EditText) findViewById(R.id.editTextTextPassword)).getText().toString();
+
+        if(editTextTextPersonName.trim().length() > 0 && !editTextTextPersonName.isEmpty() &&
+                editTextTextPassword.trim().length() > 0 && !editTextTextPassword.isEmpty()){
+//            Intent intent = new Intent(this, Gallery.class);
+//            intent.putExtra("EXTRA_USERNAME", editTextTextPersonName);
+//            startActivity(intent);
+        } else {
+//            showEmptyFieldsDialog();
+        }
     }
 }
